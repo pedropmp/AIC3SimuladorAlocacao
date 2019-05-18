59da8247c3c275dd907ccebc062531af0ac9905f
augustocsetti <38505321+augustocsetti@users.noreply.github.com>
2019-05-17 21:21:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -24,63 +24,52 @@ def inicializacao():
         inteiros.append(linha)
         
         
-def checa_lista(lista_programas, ciclo):
-    entra = []
+def checa_lista():
     # percorre lista de programas e adiciona os que entram no ciclo a lista
-    for i in range(0, len(lista_programas), 1):
+    for i in range(0, len(lista_programas)):
         if lista_programas[1] == ciclo:
-            entra.append(lista_programas)
-    return (entra)
+            fila_principal.append(lista_programas)
+            fila_principal[-1].append(0)
+            fila_principal[-1].append(0)
 
-def checa_inicio_fila_principal (lista_principal):
+
+def checa_inicio_fila_principal ():
+    # checa se há programas na lista principal
     if len(lista_principal) != 0:
-        return (0)
+        return (True)
     else:
-        return(1)
+        return(False)
 
-'''def ff (lista_principal, memoria):
-    pos, qnt = 0
-    aux = [0,0]
-    vazio = []
+
+def ff ():
+    pos_memo, qnt = 0
     # percorre lista principal
-
-    for cont in range (0, len(memoria)):
-        if memoria[cont] == 0:
-            if aux[1] == 0:
-                aux[0] = cont
-                aux[1] = 1
+    for pos_programa in range (0, len(lista_principal)):
+        # percorre memória buscando primeiro espaço para alocar processo
+        for cont in range (0, len(memoria)):
+            if memoria[cont] == 0:
+                if qnt != 0:
+                    qnt += 1
+                else:
+                    pos_memo = cont
+                    qnt = 1
+                # verifica se programa pode ser alocado no espaço
+                if qnt == lista_principal[pos_programa][prog[3]]:
+                    aloca(pos_programa, pos_memo)
             else:
-                aux[1] += 1
-        else:
-            if aux[1] != 0:
-                vazio.append(aux)
+                qnt = 0
+        lista_principal[pos_programa][5] += 1
 
-    for prog in range (0, len(lista_principal)):
-        for espaco in
 
-    for cont in range (0, len(memoria)):
-        # verifica se espaço da memória x está vazio
-        if memoria[cont] == 0:
-            pos = cont
-            qnt += 1
-            # verifica se programa pode ser alocado
-            if qnt == lista_principal:
-                return(True, pos, qnt)
-
-        else:
-            pos = cont
-            qnt = 0
-
-    return(False, 0)'''
-
-def aloca (lista_principal, executando, pos_programa, pos_memo, memoria):
+def aloca (pos_programa, pos_memo):
     lista_principal[pos_programa].append(pos_memo)
     executando.append(lista_principal[pos_programa])
-    lista_principal.pop(pos)
+    lista_principal.pop(pos_programa)
     for i in range (0, executando[-1][3]):
         memoria[pos + i] = 1
 
-def executa (executando):
+
+def executa ():
     for i in range (0, len(executando)):
         executando[i][3] -= 1
         if executando[i][3] == 0:
@@ -88,5 +77,8 @@ def executa (executando):
                 memoria[lista_principal[pos_programa][4]+j] = 0
             executado.append(executando[i])
             executando.pop(i)
+    for i in range (0, len(lista_principal)):
+        lista_principal[i][4] += 1
+
             
             
